c481b5a26962da0cbadec5ab1bdbc153fb914a14
rodrigues-heric <hericleiterodrigues@gmail.com>
2023-10-12 11:38:14 -0300

    Feat: adding exercises branch

--- trabalhofinal/bin/main.ml
+++ trabalhofinal/bin/main.ml
@@ -1,4 +1,33 @@
-(** Trabalho da disciplina Semântica Formal *)
+(** Branch de estudos de OCaml *)
 
-(** Imprime uma mensagem supimpa *)
-let main () = print_endline "Hello, World!";;
+(*
+  Comandos úteis
+  - dune build __nomeprojeto__ -> compila o projeto
+  - dune exec __nomeprojeto__ -> executa o projeto compilado
+  - dune fmt -> formata os arquivos do projeto   
+*)
+
+(*
+   Infos úteis
+   - Uma lista sempre é uma lista encadeada do tipo (elemento [resto da lista])
+   - O identificador _ é o default no loop (switch ... case ... default)
+   - O operador :: é uma das opções de lista ([_, tail] == _ :: tail)
+*)
+
+(** Write a function last : 'a list -> 'a option that returns the last element of a list *)
+let rec last : 'a list -> 'a option = function
+  | [] -> None
+  | [ x ] -> Some x
+  | _ :: tail -> last tail
+;;
+
+last [ ("a", "b", "c") ]
+
+(** Last two elements of a list *)
+let rec last_two : 'a list -> ('a * 'a) option = function
+  | [] | [ _ ] -> None
+  | [ x; y ] -> Some (x, y)
+  | _ :: tail -> last_two tail
+;;
+
+last_two [ ("a", "b", "c", "d", "e") ]
